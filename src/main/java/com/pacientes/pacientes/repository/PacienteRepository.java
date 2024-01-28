65fca47b692de8e5357c50510ca19ed46a29c180
alfonsoroc <alfonsoroc789@gmail.com>
2024-01-27 18:53:19 -0700

    PacienteRepository

--- src/main/java/com/pacientes/pacientes/repository/PacienteRepository.java
+++ src/main/java/com/pacientes/pacientes/repository/PacienteRepository.java
@@ -4,10 +4,15 @@ import com.pacientes.pacientes.entitys.Pacientes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Pacientes,Integer> {
 
-    Optional<Pacientes> findBynombrePaciente(String nombrePaciente);
+    List<Pacientes> findBynombre(String nombre);
+
+    List<Pacientes> findBy();
+
+    Optional<Pacientes> deleteAllByidPaciente(Integer idPaciente);
 }
---
+++ src/test/java/com/pacientes/pacientes/repository/PacienteRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.pacientes.pacientes.repository;
+
+import com.pacientes.pacientes.entitys.Pacientes;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class PacienteRepositoryTest {
+    @Autowired
+    PacienteRepository repository;
+    @Test
+    public void savePaciente() throws ParseException {
+        String fechaEspecifica = "1990-01-01";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date nacimiento = sdf.parse(fechaEspecifica);
+
+        Pacientes pacientes = Pacientes.builder()
+                .nombre("Abraham")
+                .apellido("Rocha")
+                .fechaNacimiento(nacimiento)
+                .edad(25)
+                .peso(78.00f)
+                .altura(175f)
+                .telefono("6672520218")
+                .email("alfonsoroc@g.com")
+                .build();
+        repository.save(pacientes);
+    }
+    @Test
+    public void findBynombrePaciente() {
+        List<Pacientes> pacientes = repository.findBynombre("Alfonso");
+        System.out.println("Paciente = " + pacientes);
+    }
+
+    @Test
+    public void findBy(){
+        List<Pacientes> pacientesList = repository.findBy();
+        System.out.println("Pacientes = " + pacientesList);
+    }
+
+    @Test
+    @Transactional
+    public void deleteById(){
+        Pacientes pacientes = repository.deleteAllByidPaciente(10).get();
+    }
+
+}
